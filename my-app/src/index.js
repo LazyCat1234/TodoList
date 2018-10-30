884f1870a63f40f1ec0e1bd361141349007d0144
yxy <yanxiaoyun@gsafety.com>
2018-10-30 16:41:01 +0800

    commit project

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//App组件，大写字母开头
+import TodoList from './TodoList';
+
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
+
+
